f29f7fb02b98788ef7557c326ddb787608ee92f7
alex99091 <bkk99091@gmail.com>
2025-03-11 22:26:03 +0900

    FEAT: Add Model & interface

---
+++ test/src/main/java/example/test/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package example.test.repository;
+
+import example.test.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(String id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
